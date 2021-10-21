d02995e8edbd927d1e7dd461a46fa7388d7884c6
Mats Roshauw <karmats@gmail.com>
2021-10-21 12:12:21 +0200

    Vue counter

---
+++ vue-counter/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="container">
+    <Counter />
+  </div>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from "vue-class-component";
+import Counter from "./components/Counter.vue";
+
+@Options({
+  components: {
+    Counter,
+  },
+})
+export default class App extends Vue {}
+</script>
+
+<style>
+body {
+  font-family: "Gill Sans", "Gill Sans MT", Calibri, "Trebuchet MS", sans-serif;
+  font-size: 100%;
+}
+.container {
+  max-width: 1200px;
+  padding-top: 3rem;
+  margin: 0 auto;
+}
+</style>
---
+++ vue-counter/src/components/Counter.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="counter-container">
+    <h1>{{ count }}</h1>
+    <div class="button-container">
+      <button type="button" @click="onDecrement">-</button>
+      <button type="button" @click="onIncrement">+</button>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from "vue-class-component";
+
+@Options({})
+export default class Counter extends Vue {
+  count = 0;
+
+  onIncrement(): void {
+    this.count += 1;
+  }
+  onDecrement(): void {
+    this.count -= 1;
+  }
+}
+</script>
+
+<style scoped>
+.counter-container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+h1 {
+  font-size: 6rem;
+}
+
+.button-container {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  min-width: 20rem;
+}
+
+button {
+  padding: 1rem 2rem;
+  font-size: 2rem;
+  color: white;
+  background-color: #4793ff;
+  border: transparent;
+  outline: transparent;
+}
+button:hover {
+  background-color: #1f79f6;
+}
+</style>
