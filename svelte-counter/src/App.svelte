2d3acda07cf4aa08509ff7674ff12f7703aaedbe
Mats Roshauw <karmats@gmail.com>
2022-02-13 07:33:25 +0100

    Svelte counter

---
+++ svelte-counter/src/App.svelte
@@ -0,0 +1,15 @@
+<script>
+  import Counter from "./components/Counter.svelte";
+</script>
+
+<div class="container">
+  <Counter />
+</div>
+
+<style>
+  .container {
+    max-width: 1200px;
+    padding-top: 3rem;
+    margin: 0 auto;
+  }
+</style>
